cc3f46f2a69898d9e075292eef8d86c89321e0cb
Daniel Cabrera <daniel.cabrera.benitez@gmail.com>
2024-04-11 15:16:13 +0200

    chore: cleaned some code

--- src/utils/getHttpErrorMessage.ts
+++ src/utils/getHttpErrorMessage.ts
@@ -1,7 +1,6 @@
 import { DEFAULT_INTERNAL_ERROR_MESSAGE } from "../constants";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getHttpErrorMessage = (error: any) => {
-  // eslint-disable-next-line  @typescript-eslint/no-unsafe-return
   return (
     error.response?.data?.message ??
     error.message ??
