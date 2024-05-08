d4e8eefe0287a493fe17015cae379dffb67623db
Daniel Cabrera <daniel.cabrera.benitez@gmail.com>
2024-05-08 12:07:29 +0200

    feat: added uploadAsset

--- src/lib/documents/index.ts
+++ src/lib/documents/index.ts
@@ -152,3 +152,4 @@ export const addTemplate = async (
     throw new Error(`ContextSDK: ${getHttpErrorMessage(error)}`);
   }
 };
+
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,38 @@
+import {FormData} from "formdata-node";
+import {fileFromPath} from "formdata-node/file-from-path";
+import axios from "axios";
+
+import {Config} from "./types";
+import {getHttpHeaders} from "../utils/getHttpHeader";
+import {ContextError, ContextErrorReason} from "../utils/ContextError";
+import {getHttpErrorMessage} from "../utils/getHttpErrorMessage";
+
 export { getAllDocuments, getDocument, createDocument } from "./documents";
 export { getAllDomains, getDomain } from "./domains";
+export const uploadAsset = async (
+    filePath: string,
+    apiKey: string,
+    config: Config,
+): Promise<{docId:string; cost: number; kilobytes: number;}> => {
+    const url = `${config.url}/assets`;
+
+    const formData = new FormData();
+    formData.append("file", await fileFromPath(filePath));
+
+    try {
+        const response = await axios.post(url, formData, {
+            headers: {
+                ...getHttpHeaders(apiKey),
+            },
+        });
+
+        if (response.status === 403) {
+            throw new ContextError(ContextErrorReason.AuthError);
+        }
+
+        return response.data;
+    }
+    catch (error) {
+        throw new Error(`ContextSDK: ${getHttpErrorMessage(error)}`);
+    }
+}
